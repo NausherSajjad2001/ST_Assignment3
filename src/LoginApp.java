9f064c5beaa315791bd6bfa18a38a0f6b55b4862
Your Name <naushersajjad7@gmail.com>
2024-12-05 19:28:29 +0500

    Added print statement

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -77,6 +77,7 @@ public class LoginApp extends JFrame {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            System.out.print("loginApp ");
             LoginApp loginApp = new LoginApp();
             loginApp.setVisible(true);
         });
